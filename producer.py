0a9c5d9aef85ec3f68aa2a5e0e5058e03fcb4b30
Icaro Seara <ikro89@gmail.com>
2018-07-12 22:37:27 -0300

    Improved producer message

--- producer.py
+++ producer.py
@@ -13,7 +13,7 @@ def delivery_report(err, msg):
     if err is not None:
         print('Message delivery failed: {}'.format(err))
     else:
-        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+        print('Message "{}" delivered to "{}" [{}]'.format(msg.value().decode("utf-8"), msg.topic(), msg.partition()))
 
 
 print('Kafka producer started!')
